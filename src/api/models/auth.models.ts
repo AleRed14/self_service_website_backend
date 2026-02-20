8a72c20728e3cfd4f07cfcce87bb2bd03c0cc4a3
AleRed14 <alejoperez6814@gmail.com>
2026-02-20 15:53:05 -0300

    fixing query return

--- src/api/models/auth.models.ts
+++ src/api/models/auth.models.ts
@@ -1,9 +1,11 @@
 import connection from "../database/db.ts";
+import { RowDataPacket } from "mysql2";
+import { IProduct } from "../../../@types/express/index.ts";
 
 const validateUser = (email: Email) => {
     const sql = `SELECT * FROM users WHERE email = ?`;
 
-    return connection.query(sql, [email]);
+    return connection.query<RowDataPacket[] & IProduct[]>(sql, [email]);
 }
 
 export default{
